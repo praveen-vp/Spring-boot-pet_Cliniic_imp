52c8caf222ed7084d14671aa705f78758bf3c28c
praveen-vp <praveen_v.p@outlook.com>
2018-08-12 16:44:46 +0530

    initial commit

---
+++ src/main/java/com/vp/springboot/SpringBootPetCliniicImpApplication.java
@@ -0,0 +1,12 @@
+package com.vp.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPetCliniicImpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPetCliniicImpApplication.class, args);
+	}
+}
