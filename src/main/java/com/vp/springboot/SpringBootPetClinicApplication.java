8edfa931df06964ee5fcdaaafeb637169d2de042
praveen-vp <praveen_v.p@outlook.com>
2018-08-12 17:26:33 +0530

    updating pom file and application file

---
+++ src/main/java/com/vp/springboot/SpringBootPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.vp.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPetClinicApplication.class, args);
+	}
+}
