eb9ee368d70d676e66163f4f15667eeab5cf2a94
praveen-vp <praveen_v.p@outlook.com>
2018-09-15 16:34:01 +0530

    Adding files

---
+++ pet-clinic-data/src/main/java/com/vp/springboot/model/Owner.java
@@ -0,0 +1,11 @@
+package main.java.com.vp.springboot.model;
+
+import com.vp.springboot.model.Person;
+
+public class Owner extends Person {
+
+	public Owner() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
