329508b184778a227aab63bfdee889c9c5f5593a
praveen-vp <praveen_v.p@outlook.com>
2018-09-16 16:00:49 +0530

    Adding index controller closes #9

---
+++ pet-clinic-web/src/main/java/com/vp/springboot/controller/IndexController.java
@@ -0,0 +1,25 @@
+/**
+ * 16-Sep-2018
+ * praveen-vp
+ * IndexController.java		
+ */
+package com.vp.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author praveen-vp
+ * 16-Sep-2018
+ *
+ */
+@Controller
+public class IndexController {
+	
+	@RequestMapping({"", "/", "index", "index.html"})
+	public String index() {
+		
+		return "index";
+	}
+
+}
