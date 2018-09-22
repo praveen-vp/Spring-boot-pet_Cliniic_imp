ed8774e58178b4fe63d9ba0afc6c0bcb8af884ac
praveen <praveenv.p@outlook.com>
2018-09-22 18:53:58 +0530

    adding view owner

--- pet-clinic-web/src/main/java/com/vp/springboot/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/vp/springboot/controller/OwnerController.java
@@ -1,14 +1,25 @@
 package com.vp.springboot.controller;
 
+import com.vp.springboot.services.OwnerServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("owmers")
 @Controller
 public class OwnerController {
 
+    private final OwnerServices ownerServices;
+
+    public OwnerController(OwnerServices ownerServices) {
+        this.ownerServices = ownerServices;
+    }
+
     @RequestMapping({"", "/index.html", "/index"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerServices.findAll());
+
         return "owners/index.html";
     }
 }
