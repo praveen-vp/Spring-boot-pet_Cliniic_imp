29ecb1871dee5c44e4e9fad390deda7d4856d132
praveen-vp <praveen_v.p@outlook.com>
2018-10-07 13:53:37 +0530

    added pettype in bootStrap closes #31

--- pet-clinic-web/src/main/java/com/vp/springboot/controller/DataLoader.java
+++ pet-clinic-web/src/main/java/com/vp/springboot/controller/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.vp.springboot.model.Owner;
+import com.vp.springboot.model.PetType;
 import com.vp.springboot.model.Vet;
 import com.vp.springboot.services.OwnerServices;
+import com.vp.springboot.services.PetTypeService;
 import com.vp.springboot.services.VetService;
 import com.vp.springboot.services.map.OwnerServiceMap;
 import com.vp.springboot.services.map.VetServiceMap;
@@ -15,15 +17,25 @@ public class DataLoader implements CommandLineRunner{
 	
 	private final OwnerServices ownerServices;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-	public DataLoader(OwnerServices ownerServices, VetService vetService) {
+	public DataLoader(OwnerServices ownerServices, VetService vetService, PetTypeService petTypeService) {
 		this.ownerServices = ownerServices;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 
+		PetType dog = new PetType();
+		dog.setName("dog");
+		PetType savedPetDogType = petTypeService.Save(dog);
+
+		PetType cat = new PetType();
+		cat.setName("cat");
+		PetType savedPetcatType = petTypeService.Save(cat);
+		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Mic");
 		owner1.setLastName("Last");
