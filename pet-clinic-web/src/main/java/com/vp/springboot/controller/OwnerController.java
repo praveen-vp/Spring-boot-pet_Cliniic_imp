2b4a651ee6934f77c3a1e0db49dcf36d80187705
praveen <praveenv.p@outlook.com>
2019-01-12 14:55:49 +0530

    changes in owner controller

--- pet-clinic-web/src/main/java/com/vp/springboot/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/vp/springboot/controller/OwnerController.java
@@ -1,31 +1,1 @@
-package com.vp.springboot.controller;
-
-import com.vp.springboot.services.map.OwnerServiceMap;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("owners")
-@Controller
-public class OwnerController {
-
-    private final OwnerServiceMap ownerServices;
-
-    public OwnerController(OwnerServiceMap ownerServices) {
-        this.ownerServices = ownerServices;
-    }
-
-    @RequestMapping({"", "/index.html", "/index"})
-    public String listOwners(Model model) {
-
-        model.addAttribute("owners", ownerServices.findAll());
-
-        return "owners/index.html";
-    }
-    
-    @RequestMapping("/find")
-    public String find() {
-    	
-    	return "notimplemented";
-    }
-}
+package com.vp.springboot.controller;import com.vp.springboot.services.map.OwnerServiceMap;import org.springframework.stereotype.Controller;import org.springframework.ui.Model;import org.springframework.web.bind.annotation.RequestMapping;@RequestMapping("owners")@Controllerpublic class OwnerController {    private final OwnerServiceMap ownerServices;    public OwnerController(OwnerServiceMap ownerServices) {        this.ownerServices = ownerServices;    }    @RequestMapping({"", "/index.html", "/index"})    public String listOwners(Model model) {        model.addAttribute("owners", ownerServices.findAll());        return "owners/index.html";    }    @RequestMapping("/find")    public String find() {        return "notimplemented";    }}
