a0ce5bbb6df01c6f7bc02a29494dda9ceb074143
praveen <praveenv.p@outlook.com>
2019-01-13 18:13:59 +0530

    implemented the Pet jpa services closes #40

---
+++ pet-clinic-data/src/main/java/com/vp/springboot/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,44 @@
+package com.vp.springboot.services.springdatajpa;
+
+import com.vp.springboot.model.Pet;
+import com.vp.springboot.repositories.PetRepository;
+import com.vp.springboot.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        HashSet<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet Save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
